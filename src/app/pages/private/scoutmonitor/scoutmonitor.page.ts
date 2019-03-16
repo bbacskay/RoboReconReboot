0760fd1288d81bfae85a03d15d280352e1a46484
Balazs Bacskay <bacskay.balazs@gmail.com>
2019-03-16 15:47:14 -0400

    Adding the events name to monitoring header.

--- src/app/pages/private/scoutmonitor/scoutmonitor.page.ts
+++ src/app/pages/private/scoutmonitor/scoutmonitor.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MonitorMatchData, MonitorQuestionItem } from '../../../interfaces/match';
 import { MatchscoutingDataService } from '../../../services/matchscouting-data.service';
+import { EventsService } from '../../../services/events.service';
+import { ConfigService } from '../../../services/config.service';
 
 @Component({
   selector: 'app-scoutmonitor',
@@ -10,13 +12,37 @@ import { MatchscoutingDataService } from '../../../services/matchscouting-data.s
 export class ScoutmonitorPage implements OnInit {
 
   public matches: MonitorMatchData[] = [];
+  public event: string;
 
-  constructor(private scoutingData: MatchscoutingDataService) { 
+  constructor(private scoutingData: MatchscoutingDataService,
+              private config: ConfigService,
+              private eventlist: EventsService
+              ) { 
     
     this.scoutingData.monitorData.subscribe((data) => {
       this.matches = data;
       this.matches.sort((a,b) => b.match_no - a.match_no);
     });
+
+    this.eventlist.events.subscribe((data) => {
+      var i = 0;
+      var eventFound = false;
+      this.event = '';
+
+      if (data.length > 0) {
+        do {
+          if (data[i].id == this.config.config.selectedEvent) {
+            this.event = data[i].name;
+            eventFound = true;
+          } else {
+            i++;
+          }
+
+        } while ((i < data.length) && (!eventFound));
+
+      }
+    });
+
   }
 
   ngOnInit() {
